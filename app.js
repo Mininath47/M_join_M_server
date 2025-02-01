a4543bcd60134b15b62a2b06040744f08b3ee281
Mininath47 <155306352+Mininath47@users.noreply.github.com>
2025-02-01 13:17:39 +0530

    m

--- app.js
+++ app.js
@@ -41,14 +41,80 @@ app.get('/user',(req,res)=>{
         database.collection('users').find().toArray().then((document)=>{
             res.send(document);
             res.end();
-        })
-    })
+        });
+    }) ;
+});
+
+app.get('/dairy',(req,res)=>{
+    mongodb.connect(MongoPath).then((object)=>{
+        const database = object.db('e-com');
+        database.collection('dairy').find().toArray().then((document)=>{
+            res.send(document);
+            res.end();
+        });
+    }) ;
+});
+
+app.get('/driks',(req,res)=>{
+    mongodb.connect(MongoPath).then((object)=>{
+        const database = object.db('e-com');
+        database.collection('drinks').find().toArray().then((document)=>{
+            res.send(document);
+            res.end();
+        });
+    }) ;
+});
+app.get('/foods',(req,res)=>{
+    mongodb.connect(MongoPath).then((object)=>{
+        const database = object.db('e-com');
+        database.collection('foods').find().toArray().then((document)=>{
+            res.send(document);
+            res.end();
+        });
+    }) ;
+});
+app.get('/vegetarian',(req,res)=>{
+    mongodb.connect(MongoPath).then((object)=>{
+        const database = object.db('e-com');
+        database.collection('vegetarian').find().toArray().then((document)=>{
+            res.send(document);
+            res.end();
+        });
+    }) ;
+});
+app.get('/product',(req,res)=>{
+    mongodb.connect(MongoPath).then((object)=>{
+        const database = object.db('e-com');
+        database.collection('product').find().toArray().then((document)=>{
+            res.send(document);
+            res.end();
+        });
+    }) ;
+});
+app.get('/login',(req,res)=>{
+    mongodb.connect(MongoPath).then((object)=>{
+        const database = object.db('login');
+        database.collection('user').find().toArray().then((document)=>{
+            res.send(document);
+            res.end();
+        }); 
+    }) ;
 });
 
 //Mysql 
 
 app.get('/users',(req,res)=>{
    db.query('select * from user',(err,result)=>{
+    if(err)
+    {
+        throw err;
+    }
+    res.send(result)
+   })
+});
+
+app.get('/image',(req,res)=>{
+   db.query('select * from image',(err,result)=>{
     if(err)
     {
         throw err;
